d5d47c5d0ee090391752b6bbe26104c2e9fe1998
Elena Korkeamaki <elena.korkeamaki19@gmail.com>
2020-01-22 10:55:58 +0200

    updates

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -6,8 +6,13 @@ class Search {
        this.openButton = $(".js-search-trigger");
        this.closeButton = $(".search-overlay__close");
        this.searchOverlay = $(".search-overlay");
+       this.searchField =  $("#search-term");
        this.events();
        this.isOverlayOpen = false;
+       this.isSprinnerVisible = false;
+       this.resultsDiv = $("#search-overlay__results");
+       this.previousValue;
+
     }
 
     //2. events
@@ -15,9 +20,37 @@ class Search {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
         $(document).on("keydown", this.keyPressSispatcher.bind(this));
+        this.searchField.on("keyup", this.typingLogic.bind(this));
+        this.typingTimer;
     }
 
     //3. methods (function, action...)
+    typingLogic() {
+        if(this.searchField.val() != this.previousValue) {
+            clearTimeout(this.typingTimer);
+
+            if(this.searchField.val()) {
+                if(!this.isSprinnerVisible) {
+                    this.resultsDiv.html('<div class="spinner-loader"></div>')
+                    this.isSprinnerVisible = true;
+                }
+                this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+            } else {
+                this.resultsDiv.html('');
+                this.isSprinnerVisible = false;
+            }
+
+            
+        }
+       
+        this.previousValue = this.searchField.val();
+    }
+
+    getResults() {
+        this.resultsDiv.html("Imagine real search results here...");
+        this.isSprinnerVisible = false;
+    }
+    
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll");
